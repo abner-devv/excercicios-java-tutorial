61d46e9955397897166c57dd214333325bd0e315
abner-santana-dev <abner.santana.dev@gmail.com>
2026-02-25 12:00:50 -0300

    refactor files

--- com.javadateandtime.br/src/DataEHora1.java
+++ com.javadateandtime.br/src/DataEHora1.java
@@ -87,7 +87,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
-public class ExercicioDataEHora1 {
+public class DataEHora1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- com.javadateandtime.br/src/DataEHora2.java
+++ com.javadateandtime.br/src/DataEHora2.java
@@ -83,7 +83,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
-public class ExercicioDataEHora2 {
+public class DataEHora2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
