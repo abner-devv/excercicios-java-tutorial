81c15ed44ceceb1618b8c6aa5d8e1df4d06c2a1b
abner-santana-dev <abner.santana.dev@gmail.com>
2026-03-03 21:09:42 -0300

    hashset

--- br.com.abner.exercicios.hashset/src/Main.java
+++ br.com.abner.exercicios.hashset/src/Main.java
@@ -20,8 +20,19 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        HashSet<String> nomes = new HashSet<>();
+        nomes.add("pedro");
+        nomes.add("lucas");
+        nomes.add("joao");
+        nomes.add("sanderson");
+        nomes.add("vitor");
+        nomes.add("vitor");
 
-        // Seu código aqui
+        for(String nome : nomes){
+            System.out.println(nome);
+        }
 
+        System.out.println("Quantidade total: " + nomes.size());
+        System.out.println("Buscando por (joao): " + nomes.contains("joao"));
     }
 }
